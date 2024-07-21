2044ea212ced460c23be454ae3bcd3b0ef370932
samanes7 <samaneshaghi7@gmail.com>
2024-07-21 17:01:43 +0330

    App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProfileList from './components/ProfileList';
+
+function App() {
+  return (
+    <div className="App">
+      <ProfileList />
+    </div>
+  );
+}
+
+export default App;
